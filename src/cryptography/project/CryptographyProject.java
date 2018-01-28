d4e0776ace3109a0fe1c7d594072e0be9a60ad04
NitishOritro <nitish.csedu51@gmail.com>
2018-01-28 17:12:20 +0600

    Generate 2d wise ciphertext

--- src/cryptography/project/CryptographyProject.java
+++ src/cryptography/project/CryptographyProject.java
@@ -7,6 +7,7 @@ package cryptography.project;
 
 import com.letter.cyptography.LetterInNumber;
 import static java.lang.Character.isLetter;
+import static java.lang.Character.isWhitespace;
 import java.util.Random;
 
 /**
@@ -31,8 +32,10 @@ public class CryptographyProject
         System.out.println("Number conversion is in Character "+numberInLetterCheck.getLetter());
         
         //Random rand = new Random();
-        int i,j,k;
+        int i,j,k,row=16,column=0;
+        
         String ciphertext = "";
+        
         for(i=0;i<plaintext.length();i++)
         {
             if(Character.isLetter(plaintext.charAt(i)) && plaintext.charAt(i) != ' ')
@@ -44,9 +47,52 @@ public class CryptographyProject
                 char cipherCharacter = numberInLetter.getLetter();
                 ciphertext = ciphertext + cipherCharacter;
             }
+            else if(plaintext.charAt(i) != ' ' || Character.isWhitespace(plaintext.charAt(i)))
+            {
+                ciphertext = ciphertext + plaintext.charAt(i);
+            }
         }
-        System.out.println("CipherText is" +ciphertext);
+        System.out.println("CipherText is : " +ciphertext);
         
+        if(plaintext.length() % 16 == 0)
+        {
+            column = plaintext.length() / 16;
+        }
+        else 
+        {
+            column = plaintext.length() / 16 + 1;
+        }
+        System.out.println("Column is : " +column);
+        
+        //Initialize a 2d array
+        char ciphertextRotor [][] = new char [row][column];
+        int pointer = 0;
+        for(i=0;i<column;i++)
+        {
+            for(j=0;j<row;j++)
+            {
+                if(pointer != ciphertext.length())
+                {
+                    ciphertextRotor [j][i] = ciphertext.charAt(pointer);
+                    pointer++;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+            System.out.println(pointer);
+        }
+        
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<column;j++)
+            {
+                //ciphertextRotor [row][column] = plaintext.charAt(j);
+                 System.out.print(" ("+i+" "+j+")"+ciphertextRotor [i][j]+"   ");
+            }
+            System.out.println();
+        }
     }
     
 }
